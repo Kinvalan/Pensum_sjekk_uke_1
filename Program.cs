17f49ff19654c7074c46a2ec172ade3b9d3a1af6
Kinvalan <sebastianmoller01@icloud.com>
2023-05-07 19:20:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Lag en app som tildeler en person en random hobby
+
+Skriv navn inn i konsollen,
+la en funksjon trekke en tilfeldig hobby - 
+og vis dette sammen med navnet i konsollvinduet.
+
+La det være 3-5 forskjellige hobbyer som kan trekkes.
+*/
+
+
+namespace Pensum_sjekk_uke_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv inn et navn: ");
+            randomHobby();
+        }
+
+        static void randomHobby()
+        {
+            string name = Console.ReadLine();
+            string[] hobbies = { "strikking", "tegning", "fotografering" };
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, hobbies.Length);
+
+            string randomHobbyString = hobbies[randomNumber];
+            Console.WriteLine(name + " du fikk " + randomHobbyString + " som hobby!");
+        }
+    }
+}
